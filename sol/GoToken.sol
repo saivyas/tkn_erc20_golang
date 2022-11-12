f7edb602df92407b311d95f7e9ea18f681151aa3
vyas-git <vyasreddy.tech@gmail.com>
2022-11-12 23:57:09 +0530

    solidity for token generation

---
+++ sol/GoToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "./openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract GoToken is ERC20 {
+    constructor(uint256 totalSupply) ERC20("GoToken", "GOT") {
+        _mint(msg.sender, totalSupply);
+    }
+}
