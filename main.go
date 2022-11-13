a8e62b199380649ad081f5718f0d10a215a18922
vyas-git <vyasreddy.tech@gmail.com>
2022-11-13 14:55:22 +0530

    token deploy using polygon test net

--- main.go
+++ main.go
@@ -2,23 +2,43 @@ package main
 
 import (
 	"context"
-	"crypto/ecdsa"
+	"fmt"
 	"log"
 	"math/big"
+	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
 
 	GoToken "github.com/vyas-git/tkn_erc20_golang/go-gen"
 )
 
+var myenv map[string]string
+
+const envLoc = ".env"
+
 func main() {
 	ctx := context.Background()
+	client, err := ethclient.Dial(os.Getenv("GATEWAY")) //address of testnet
+	if err != nil {
+		log.Fatalf("could not connect to Ethereum gateway: %v\n", err)
+	}
 	session := NewSession(ctx)
+
+	totalSupply := new(big.Int)
+	totalSupply.SetString("100000000000000000000000", 10)
+	NewTokenDeploy(session, client, totalSupply)
 
 }
 
 func NewSession(ctx context.Context) (session GoToken.GoTokenSession) {
-	var pk *ecdsa.PrivateKey
+	pk, err := crypto.HexToECDSA(os.Getenv("PRIVATE_KEY"))
+
+	if err != nil {
+		log.Println("Error getting private key: ", err)
+	}
 
 	auth, err := bind.NewKeyedTransactorWithChainID(pk, big.NewInt(5))
 
@@ -34,6 +54,39 @@ func NewSession(ctx context.Context) (session GoToken.GoTokenSession) {
 		CallOpts:     callOpts,
 	}
 }
-func NewTokenDeploy() {
+func NewTokenDeploy(session GoToken.GoTokenSession, client *ethclient.Client, TotalSupply *big.Int) GoToken.GoTokenSession {
+	log.Println("Creating new Token contract\n ")
+	loadEnv()
 
+	// deploy contract to get the contract addr, transaction object, adnd instance
+	contractAddress, tx, instance, err := GoToken.DeployGoToken(&session.TransactOpts, client, TotalSupply)
+	if err != nil {
+		log.Fatalf("could not deploy token contract: %v\n", err)
+	}
+
+	//print the adress of the transaction - this can be used to up on ethresan the progress of the transaction
+	fmt.Println("Contract deployed! Wait for tx %s to be confirmed.\n ", tx.Hash().Hex())
+
+	session.Contract = instance
+
+	//save the address of the deployed contract
+	updateEnvFile("TOKEN_CONTRACTADDR", contractAddress.Hex())
+
+	return session
+}
+func loadEnv() {
+	var err error
+	if err = godotenv.Load(envLoc); err != nil {
+		log.Printf("could not load env from %s: %v", envLoc, err)
+	}
+
+	if myenv, err = godotenv.Read(envLoc); err != nil {
+		log.Printf("could not read in env from %s: %s", envLoc, err)
+	}
+}
+func updateEnvFile(key, val string) {
+	myenv[key] = val
+	if err := godotenv.Write(myenv, envLoc); err != nil {
+		log.Printf("failed to update %s: %s", envLoc, err)
+	}
 }
